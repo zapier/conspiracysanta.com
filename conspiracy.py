e55a6233f4aa9fef3adb16dbb2c85d9daf4f0d97
Bryan Helmig <bryan@bryanhelmig.com>
2014-11-14 12:12:01 -0800

    add ze bootstrap and other goodies!

--- conspiracy.py
+++ conspiracy.py
@@ -1,11 +1,31 @@
 import os
 import bottle
 
-@bottle.route('/')
-def hello_world():
-    return '<h1>Hello Bottle!</h1>'
+from bottle import run, get, static_file, route
+
+@get('/js/<filename:re:.*\.js>')
+def javascripts(filename):
+    return static_file(filename, root='assets/js')
+
+@get('/css/<filename:re:.*\.css>')
+def stylesheets(filename):
+    return static_file(filename, root='assets/css')
+
+@get('/img/<filename:re:.*\.(jpg|png|gif|ico)>')
+def images(filename):
+    return static_file(filename, root='assets/img')
+
+@get('/fonts/<filename:re:.*\.(eot|ttf|woff|svg)>')
+def fonts(filename):
+    return static_file(filename, root='assets/fonts')
+
+
+@route('/')
+def home():
+    return static_file('base.html', root='assets/html')
+
 
 if __name__ == '__main__':
-    bottle.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 app = bottle.default_app()
